763fffedb17f52262629c9bfd47728f6a8711143
dcatanor <dasniel199410@gmail.com>
2021-11-24 20:16:20 -0500

    Sprint 3

--- spec/src.spec.js
+++ spec/src.spec.js
@@ -3,53 +3,50 @@ describe('src', () => {
 
     describe('Tests on registrarAQI', function () {
         it('should be \'excelente\'', () => {
-            expect(Src.registrarAQI(3)).toBe('excelente');
-            expect(Src.registrarAQI(0)).toBe('excelente');
-            expect(Src.registrarAQI(30)).toBe('excelente');
+            Src.registrarAQI(3).then(val => expect(val).toBe('excelente'))
+            Src.registrarAQI(0).then(val => expect(val).toBe('excelente'))
+            Src.registrarAQI(30).then(val => expect(val).toBe('excelente'))
         });
         it('should be \'bueno\'', () => {
-            expect(Src.registrarAQI(40)).toBe('bueno');
-            expect(Src.registrarAQI(31)).toBe('bueno');
-            expect(Src.registrarAQI(50)).toBe('bueno');
+            Src.registrarAQI(40).then(val => expect(val).toBe('bueno'));
+            Src.registrarAQI(31).then(val => expect(val).toBe('bueno'));
+            Src.registrarAQI(50).then(val => expect(val).toBe('bueno'));
         });
         it('should be \'moderado\'', () => {
-            expect(Src.registrarAQI(51)).toBe('moderado');
-            expect(Src.registrarAQI(80)).toBe('moderado');
-            expect(Src.registrarAQI(100)).toBe('moderado');
+            Src.registrarAQI(51).then(val => expect(val).toBe('moderado'));
+            Src.registrarAQI(80).then(val => expect(val).toBe('moderado'));
+            Src.registrarAQI(100).then(val => expect(val).toBe('moderado'));
         });
         it('should be \'bajo\'', () => {
-            expect(Src.registrarAQI(101)).toBe('bajo');
-            expect(Src.registrarAQI(150)).toBe('bajo');
-            expect(Src.registrarAQI(120)).toBe('bajo');
+            Src.registrarAQI(101).then(val => expect(val).toBe('bajo'));
+            Src.registrarAQI(150).then(val => expect(val).toBe('bajo'));
+            Src.registrarAQI(120).then(val => expect(val).toBe('bajo'));
         });
         it('should be \'insalubre\'', () => {
-            expect(Src.registrarAQI(151)).toBe('insalubre');
-            expect(Src.registrarAQI(171)).toBe('insalubre');
-            expect(Src.registrarAQI(200)).toBe('insalubre');
+            Src.registrarAQI(151).then(val => expect(val).toBe('insalubre'));
+            Src.registrarAQI(171).then(val => expect(val).toBe('insalubre'));
+            Src.registrarAQI(200).then(val => expect(val).toBe('insalubre'));
         });
         it('should be \'peligroso\'', () => {
-            expect(Src.registrarAQI(201)).toBe('peligroso');
-            expect(Src.registrarAQI(221)).toBe('peligroso');
-            expect(Src.registrarAQI(300)).toBe('peligroso');
+            Src.registrarAQI(201).then(val => expect(val).toBe('peligroso'));
+            Src.registrarAQI(221).then(val => expect(val).toBe('peligroso'));
+            Src.registrarAQI(300).then(val => expect(val).toBe('peligroso'));
         });
         it('should be \'fuera_de_rango\'', () => {
-            expect(Src.registrarAQI(-3)).toBe('fuera_de_rango');
-            expect(Src.registrarAQI(301)).toBe('fuera_de_rango');
+            Src.registrarAQI(-3).then(val => expect(val).toBe('fuera_de_rango'));
+            Src.registrarAQI(301).then(val => expect(val).toBe('fuera_de_rango'));
         });
     });
 
     describe('tests on calcularPorcentajes', () => {
         it('sould be 100%',  () => {
-           const value = Src.calcularPocentajes(1, 1, 1, 1, 1, 1,  1, 1, 1, 1);
-           expect(value.porcentajePM10).toBeCloseTo(100)
+           Src.calcularPocentajes(1, 1, 1, 1, 1, 1,  1, 1, 1, 1).then(value => expect(value.porcentajePM10).toBeCloseTo(100));
         });
         it('sould be 0',  () => {
-            const value = Src.calcularPocentajes(0, 0, 0, 0, 0, 1,  1, 1, 1, 1);
-            expect(value.porcentajePM10).toBeCloseTo(0)
+            Src.calcularPocentajes(0, 0, 0, 0, 0, 1,  1, 1, 1, 1).then(value => expect(value.porcentajePM10).toBeCloseTo(0));
         });
         it('sould calculate variable percentages',  () => {
-            const value = Src.calcularPocentajes(40, 100, 100, 100, 100, 10,  20, 30, 40, 50);
-            expect(value.porcentajePM10).toBeCloseTo(25.001)
+            Src.calcularPocentajes(40, 100, 100, 100, 100, 10,  20, 30, 40, 50).then(value => expect(value.porcentajePM10).toBeCloseTo(25.001));
         });
     })
 });
--- src.js
+++ src.js
@@ -1,32 +1,26 @@
 const calcularPorcentaje = (valor, limite) => {
-    console.log(valor, limite);
-    const result = limite ? (valor / limite) * 100 : 0;
-    console.log('result', result);
-    return result;
+    console.log(valor, limite, valor / limite);
+    return limite ? (valor / limite) * 100 : 0;
 };
 
-const calcularPorcentajes = (limitePM10, limitePM2punto5, limiteNO2, limiteCO, limiteS02, valorPM10, valorPM2punto5, valorNO2, valorCO, valorSO2) => {
+const calcularPorcentajes = async (valorPM10, valorPM2punto5, valorNO2, valorCO, valorSO2) => {
+    let response = await fetch('https://misiontic2022upb.vercel.app/api/air-quality/limits');
+    let limits = await response.json();
     return {
-        porcentajePM10: calcularPorcentaje(valorPM10, limitePM10),
-        porcentajePMpunto2: calcularPorcentaje(valorPM2punto5, limitePM2punto5),
-        porcentajeNO2: calcularPorcentaje(valorNO2, limiteNO2),
-        porcentajeCO: calcularPorcentaje(valorCO, limiteCO),
-        porcentajeSO2: calcularPorcentaje(valorSO2, limiteS02)
+        porcentajePM10: calcularPorcentaje(valorPM10, limits.limitePM10),
+        porcentajePMpunto2: calcularPorcentaje(valorPM2punto5, limits.limitePM2punto5),
+        porcentajeNO2: calcularPorcentaje(valorNO2, limits.limiteNO2),
+        porcentajeCO: calcularPorcentaje(valorCO, limits.limiteCO),
+        porcentajeSO2: calcularPorcentaje(valorSO2, limits.limiteS02)
     }
 }
 
-const registrarAQI = value => {
+const registrarAQI = async value => {
+    let response = await fetch('https://misiontic2022upb.vercel.app/api/air-quality/aqi-ranges');
+    let rangosAQI = await response.json();
     const rango = rangosAQI.find(rangoAQI => value >= rangoAQI.de && value <= rangoAQI.hasta);
     return rango ? rango.etiqueta : 'fuera_de_rango';
 }
 
 module.exports.registrarAQI = registrarAQI;
 module.exports.calcularPocentajes = calcularPorcentajes;
-global.rangosAQI = [
-    { etiqueta: 'excelente', de: 0, hasta: 30},
-    { etiqueta: 'bueno', de: 31, hasta: 50},
-    { etiqueta: 'moderado', de: 51, hasta: 100},
-    { etiqueta: 'bajo', de: 101, hasta: 150},
-    { etiqueta: 'insalubre', de: 151, hasta: 200},
-    { etiqueta: 'peligroso', de: 201, hasta: 300},
-];
